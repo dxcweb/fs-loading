7de6ea1a0228e51960ddaee3159ca00560963f29
dxcweb <dxcweb@qq.com>
2017-04-14 14:45:51 +0800

    no message

--- src/native/components/Loading.js
+++ src/native/components/Loading.js
@@ -5,35 +5,36 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     Modal,
-    Text
+    Text,
+    ActivityIndicator
 } from 'react-native';
 import Block from 'fs-flex'
-import LoadingIcon from './LoadingIcon'
 const styles = StyleSheet.create({
     main: {
-        width: 190,
-        height: 108,
-        borderRadius: 5,
+        width: 78,
+        height: 78,
+        borderRadius: 10,
         padding: 15,
-        backgroundColor: "rgba(1, 1, 1, 0.6)"
+        backgroundColor: "rgba(1, 1, 1, 0.8)"
     },
     text: {
-        color: "#fff",
+        color: "#fff"
     }
 });
 class Loading extends Component {
     render() {
-        const {title='数据加载中',isShow=true}=this.props;
+        const {isShow=true}=this.props;
         return <Modal
             visible={isShow}
             transparent={true}
         >
             <Block f={1} a="c" j="c">
                 <Block style={styles.main} a="c" vh j="c">
-                    <Block h={54}>
-                        <LoadingIcon />
-                    </Block>
-                    <Text style={styles.text}>{title}</Text>
+                    <ActivityIndicator
+                        color="white"
+                        size="large"
+                        style={{ flex: 1 }}
+                    />
                 </Block>
             </Block>
         </Modal>
